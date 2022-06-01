09bef1aec63104a1e9c8e1926b25c8519b27d947
Vivek Singh <petmongrels@gmail.com>
2022-06-01 16:26:14 +0530

    .

--- git.sh
+++ git.sh
@@ -16,3 +16,29 @@ git push -u origin xss-bug-fixes # push to new remote branch
 #tag
 git tag -a v3.17.4
 git push origin v3.17.4
+
+#openchs_repos = openchs-client openchs-server infra rules-config openchs-reporting lbp-arogyadoot ashwini calcutta-kids tt-ncd-screening sewa-rural jss unicef-moha kalap auth
+#define _git_multiple
+#	@for repo in $(openchs_repos) ; do \
+#		echo $$repo ; \
+#		git --git-dir=/Users/vsingh/Projects/samanvay/openchs/$$repo/.git --work-tree=/Users/vsingh/Projects/samanvay/openchs/$$repo $1 ; \
+#	done
+#endef
+#
+#openchs_status_all:
+#	$(call _git_multiple,status -s)
+#
+#openchs_overwrite_from_repo:
+#	$(call _git_multiple,checkout .)
+#
+#openchs_switch_to_master:
+#	$(call _git_multiple,checkout master)
+#
+#openchs_pull_all:
+#	$(call _git_multiple,pull)
+#
+#openchs_commit_stat:
+#	@for repo in $(openchs_repos) ; do \
+#		echo $$repo ; \
+#		git --git-dir=/Users/vsingh/Projects/samanvay/openchs/$$repo/.git --work-tree=/Users/vsingh/Projects/samanvay/openchs/$$repo shortlog -s master@{$(from)}...master@{$(to)} ; \
+#	done
--- mac/apache.sh
+++ mac/apache.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
+
 #DocumentRoot = /Library/WebServer/Documents
 #config = /etc/apache2
+
+/usr/local/etc/httpd/httpd.conf
+/usr/local/var/www
